75d33b846fc727dab4f3dead5772c3d281339793
ShamirAli55 <shamirali9779@gmail.com>
2025-07-21 10:26:21 -0700

    Refactor HTML structure and update CSS styles for improved layout and design consistency

--- Files/script.js
+++ Files/script.js
@@ -1,5 +1,6 @@
 var crsr = document.querySelector("#cursor");
-document.addEventListener("mousemove",(dets)=>{
+document.addEventListener("mousemove",(dets)=>
+{
      crsr.style.left = dets.x - 10+ "px";
      crsr.style.top = dets.y  - 20 + "px";
 })
