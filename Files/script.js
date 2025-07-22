7f83a29f1550da7c84c62edd3f6d570ce37adc07
ShamirAli55 <shamirali9779@gmail.com>
2025-07-22 01:52:30 -0700

    Enhance layout and styling of main and about sections; improve cursor functionality and add z-index for layering

--- Files/script.js
+++ Files/script.js
@@ -1,6 +1,17 @@
 var crsr = document.querySelector("#cursor");
-document.addEventListener("mousemove",(dets)=>
+document.addEventListener("mousemove", (dets) => {
+  crsr.style.left = dets.x - 10 + "px";
+  crsr.style.top = dets.y - 20 + "px";
+});
+
+let moon = document.querySelector("#moon");
+let train = document.querySelector("#train");
+let text = document.getElementById("#text");
+
+
+window.addEventListener('scroll', () => 
 {
-     crsr.style.left = dets.x - 10+ "px";
-     crsr.style.top = dets.y  - 20 + "px";
-})
+  let value = window.scrollY;
+  text.style.top = value * 0.5 + 'px';
+    text.style.left = value * 0.5 + 'px';
+});
